dc5e5b18e4de3a8ab1a2b6825687072706224a8d
david <david19931217@gmail.com>
2017-12-25 12:49:44 +0000

    change footer

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="footer">
     <div class="container">
         <p class="pull-left">
-            由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">David</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+            Powered By <a href="https://laravel.com" target="_blank">Laravel</a> <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="http://www.miitbeian.gov.cn" target="_blank">皖ICP备17001287号-1</a></p>
+        <p class="pull-right">© DAVIDPHP 2017. All rights reserved.<a href="http://www.miitbeian.gov.cn" target="_blank">皖ICP备17001287号-1</a></p>
     </div>
 </footer>
