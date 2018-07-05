896a37849a47b6e42f0382ae5703cf8b923a6065
david <david19931217@gmail.com>
2018-07-05 16:29:21 +0000

    add easysms

---
+++ config/easysms.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 2018/7/6
+ * Time: 0:22
+ */
+return [
+    // HTTP 请求的超时时间（秒）
+    'timeout' => 5.0,
+
+    // 默认发送配置
+    'default' => [
+        // 网关调用策略，默认：顺序调用
+        'strategy' => \Overtrue\EasySms\Strategies\OrderStrategy::class,
+
+        // 默认可用的发送网关
+        'gateways' => [
+            'yunpian',
+        ],
+    ],
+    // 可用的网关配置
+    'gateways' => [
+        'errorlog' => [
+            'file' => '/tmp/easy-sms.log',
+        ],
+        'yunpian' => [
+            'api_key' => env('YUNPIAN_API_KEY'),
+        ],
+    ],
+];
