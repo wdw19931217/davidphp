42c4388202edaaa19c3967e6e6fb0bae3482e94f
david <david19931217@gmail.com>
2018-07-06 01:55:40 +0000

    add socialiteproviders

--- config/services.php
+++ config/services.php
@@ -40,4 +40,10 @@ return [
         'key'   => env('BAIDU_TRANSLATE_KEY'),
     ],
 
+    'weixin' => [
+        'cliend_id' => env('WEIXIN_KEY'),
+        'client_secret' => env('WEIXIN_SECRET'),
+        'redirect' => env('WEIXIN_REDIRECT_URI'),
+    ],
+
 ];
