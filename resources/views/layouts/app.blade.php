bbd6deab7ccbcd87263d73e009239d60ac38313b
david <david19931217@gmail.com>
2019-01-29 10:53:27 +0800

    Add Google Analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,34 +9,49 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'DavidPHP') - {{ setting('site_name', 'DavidPHP 个人博客') }}</title>
-    <meta name="description" content="@yield('description', setting('seo_description', 'LaraBBS 爱好者社区。'))" />
-    <meta name="keyword" content="@yield('keyword', setting('seo_keyword', 'DavidPHP,社区,论坛,开发者论坛'))" />
+    <meta name="description" content="@yield('description', setting('seo_description', 'LaraBBS 爱好者社区。'))"/>
+    <meta name="keyword" content="@yield('keyword', setting('seo_keyword', 'DavidPHP,社区,论坛,开发者论坛'))"/>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('styles')
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-133442509-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'UA-133442509-1');
+    </script>
+
 </head>
 
 <body>
-    <div id="app" class="{{ route_class() }}-page">
+<div id="app" class="{{ route_class() }}-page">
 
-        @include('layouts._header')
+    @include('layouts._header')
 
-        <div class="container">
-            @include('layouts._message')
-            @yield('content')
+    <div class="container">
+        @include('layouts._message')
+        @yield('content')
 
-        </div>
-
-        @include('layouts._footer')
     </div>
 
-    @if (app()->isLocal())
-        @include('sudosu::user-selector')
-    @endif
+    @include('layouts._footer')
+</div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    @yield('scripts')
+@if (app()->isLocal())
+    @include('sudosu::user-selector')
+@endif
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+@yield('scripts')
 </body>
 </html>
